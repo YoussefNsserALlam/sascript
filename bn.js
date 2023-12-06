389cbc4b2689b2c062410ed751b06cb1fe4dfd30
YoussefNsserALlam <128973505+YoussefNsserALlam@users.noreply.github.com>
2023-12-06 16:17:30 +0200

    Update bn.js

--- bn.js
+++ bn.js
@@ -102,7 +102,7 @@ if(displayHours >= 8 && period == "مساءً"){
        document.querySelector(".te2").innerHTML = 18-displayHours;
     }
     }, 1000);
-}, 1150);
+}, 1050);
 })
         }
         document.querySelectorAll(".mobile-webview-hide a")[1].addEventListener("click", function (){
@@ -206,5 +206,5 @@ if(displayHours >= 8 && period == "مساءً"){
        document.querySelector(".te2").innerHTML = 18-displayHours;
     }
     }, 1000);
-}, 1150);
+}, 1050);
 })
