ba078751c7591e57032ac4b15f34e6021e11165d
YoussefNsserALlam <128973505+YoussefNsserALlam@users.noreply.github.com>
2024-03-10 17:33:33 +0200

    Update sh.js

--- sh.js
+++ sh.js
@@ -1,1 +1,2 @@
-console.log("hello");
+ document.querySelectorAll(".slick-track")[1].style.width = "100%"
+  document.querySelectorAll(".slider-container")[0].style.cssText = "max-width:none; width:100%";
